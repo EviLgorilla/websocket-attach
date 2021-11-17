e850c9c082acdbf49f5d0da97e51cbdc8286a18f
Mendus <alexander.mendus1@gmail.com>
2021-11-17 11:51:44 +0200

    Remove socket IO support

--- src/lib/websocket/index.ts
+++ src/lib/websocket/index.ts
@@ -2,8 +2,6 @@ export { useWebSocket as default } from './use-websocket';
 
 export type { SendMessage, Options } from './types';
 
-export { useSocketIO } from './use-socket-io';
-
 export { ReadyState } from './constants';
 
 export { useEventSource } from './use-event-source';
