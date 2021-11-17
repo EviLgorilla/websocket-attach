8ad3b804f72b31644fbaacb73594f989e082496b
Mendus <alexander.mendus1@gmail.com>
2021-11-17 11:13:23 +0200

    Migrate react-use-websocket lib

---
+++ src/lib/websocket/index.ts
@@ -0,0 +1,9 @@
+export { useWebSocket as default } from './use-websocket';
+
+export type { SendMessage, Options } from './types';
+
+export { useSocketIO } from './use-socket-io';
+
+export { ReadyState } from './constants';
+
+export { useEventSource } from './use-event-source';
